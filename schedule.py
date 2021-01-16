31384ee9929f2f561b534a3e5c15c1df8f603a64
jakwiat <kubakwiat123@gmail.com>
2021-01-16 22:40:27 +0100

    add functions to get tasks from specific date/month

--- schedule.py
+++ schedule.py
@@ -16,3 +16,21 @@ class Schedule:
             date_string = date.strftime("%d/%m/%Y %H:%M")
             print(date_string)
             self.schedule_dict[date].print_module()
+
+    def show_tasks_from_date(self, requested_date: datetime.datetime):
+        time_list = list(self.schedule_dict.keys())
+        time_list.sort()
+        for date in time_list:
+            if date.day == requested_date.day and date.month == requested_date.month and date.year == requested_date.year:
+                date_string = date.strftime("%d/%m/%Y %H:%M")
+                print(date_string)
+                self.schedule_dict[date].print_module()
+
+    def show_tasks_from_month(self, requested_date: datetime.datetime):
+        time_list = list(self.schedule_dict.keys())
+        time_list.sort()
+        for date in time_list:
+            if date.month == requested_date.month and date.year == requested_date.year:
+                date_string = date.strftime("%d/%m/%Y %H:%M")
+                print(date_string)
+                self.schedule_dict[date].print_module()
