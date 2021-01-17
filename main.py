ceb9c4681029f47eab5ea35a8daf630ea828b6da
gkaczm <gabriela.kaczmarek99@gmail.com>
2021-01-17 06:04:47 +0100

    usuwanie błędów

--- main.py
+++ main.py
@@ -6,16 +6,11 @@ from interfaces import ParentInterface, ChildInterface
 
 
 if __name__ == "__main__":
-    # task = Task("Wash your hands")
-    # module = Module([task, Task("Eat dinner")], "Dinner", "image_url", "Short description")
-    #
-    # sheldun = Schedule()
-    # sheldun.add_module(datetime.date(2021, 1, 17), module, 1)
-    # sheldun.add_module(datetime.date(2022, 5, 17), module, 1)
-    # sheldun.add_module(datetime.date(2021, 7, 17), module, 1)
-    # sheldun.add_module(datetime.date(2021, 1, 17), module, 2)
-    # sheldun.add_module(datetime.date(2021, 1, 17), module, 1)
-    # # sheldun.show_tasks_from_month(datetime.date(2020, 5, 1))
+    task = Task("Wash your hands")
+    module = Module([task, Task("Eat dinner")], "Dinner", "image_url", "Short description")
+
+
+    # sheldun.show_tasks_from_month(datetime.date(2020, 5, 1))
     # print(sheldun.schedule_dict[sheldun.today][sheldun.current_task_key])
     # sheldun.make_progress()
     # print(sheldun.schedule_dict[sheldun.today][sheldun.current_task_key])
@@ -92,7 +87,14 @@ if __name__ == "__main__":
                "DEMO: Podstawowe zadanie ścielenia łóżka."))
     default_modules_list.append(
         Module([lista, buty_on, kurtka_on, klucz, drzwi, sklep, kupuje, place, wracam, kurtka_off, buty_off],
-               "Ścielę łóżko", "image_url", "DEMO: Podstawowe zadanie ścielenia łóżka."))
+               "Wyjście do sklepu", "image_url", "DEMO: Podstawowe zadanie wyjścia do sklepu."))
+
+    Przyklad = Schedule()
+    Przyklad.add_module(datetime.date(2021, 1, 17), default_modules_list[0], 1)
+    Przyklad.add_module(datetime.date(2022, 5, 17), default_modules_list[1], 1)
+    Przyklad.add_module(datetime.date(2021, 7, 17), default_modules_list[2], 1)
+    Przyklad.add_module(datetime.date(2021, 1, 17), default_modules_list[3], 2)
+    Przyklad.add_module(datetime.date(2021, 1, 17), default_modules_list[4], 1)
 
     # start of app
     print("Witaj w aplikacji Codzienne Wyzwania! Mamy nadzieję, że masz się dobrze :)")
@@ -101,6 +103,7 @@ if __name__ == "__main__":
     user = input(">> ")
     if user == 'd':
         interface = ChildInterface(default_modules_list)
+        interface.schedule = Przyklad
     elif user == 'r':
         interface = ParentInterface(default_modules_list)
     interface.program_loop()
