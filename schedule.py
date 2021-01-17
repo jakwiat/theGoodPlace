94d80e9f4075fa11c4820f4569b30c5ac3fc608a
jakwiat <kubakwiat123@gmail.com>
2021-01-17 01:11:32 +0100

    fixed some bugs

--- schedule.py
+++ schedule.py
@@ -11,7 +11,7 @@ class Schedule:
 
     def add_module(self, date: datetime.datetime, module: Module, place: int):
         if date in self.schedule_dict:
-            self.schedule_dict[date].insert(AssignedModule(module), place - 1)
+            self.schedule_dict[date].insert(place - 1, AssignedModule(module))
         else:
             self.schedule_dict[date] = [AssignedModule(module)]
         if self.today == date:
@@ -22,9 +22,9 @@ class Schedule:
         time_list = list(self.schedule_dict.keys())
         time_list.sort()
         for date in time_list:
-            for task in self.schedule_dict[date]:
+            for task in range(len(self.schedule_dict[date])):
                 date_string = date.strftime("%d/%m/%Y")
-                print(date_string, task, sep="  -  ", end=". ")
+                print(date_string, task + 1, sep="  -  ", end=". ")
                 self.schedule_dict[date][task].print_module()
 
     def show_tasks_from_date(self, requested_date: datetime.datetime):
@@ -32,9 +32,9 @@ class Schedule:
         time_list.sort()
         for date in time_list:
             if date.day == requested_date.day and date.month == requested_date.month and date.year == requested_date.year:
-                for task in self.schedule_dict[date]:
+                for task in range(len(self.schedule_dict[date])):
                     date_string = date.strftime("%d/%m/%Y")
-                    print(date_string, task, sep="  -  ", end=". ")
+                    print(date_string, task + 1, sep="  -  ", end=". ")
                     self.schedule_dict[date][task].print_module()
                 return
 
@@ -43,9 +43,9 @@ class Schedule:
         time_list.sort()
         for date in time_list:
             if date.month == requested_date.month and date.year == requested_date.year:
-                for task in self.schedule_dict[date]:
+                for task in range(len(self.schedule_dict[date])):
                     date_string = date.strftime("%d/%m/%Y")
-                    print(date_string, task, sep="  -  ", end=". ")
+                    print(date_string, task + 1, sep="  -  ", end=". ")
                     self.schedule_dict[date][task].print_module()
 
     def make_progress(self):
